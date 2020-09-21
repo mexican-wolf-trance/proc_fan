152917856bcfac645960cef43b280c87c51e6667
Wyatt <cwdh4@umsystem.edu>
2020-09-20 20:44:22 -0500

    create the argument formatting frunction

---
+++ argFormat.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+int argFormat(const char *source, const char *delimiters, char ***newArg)
+{
+	int i, error, numtokens;
+	char *buff;
+	const char *snew;
+	
+	if ((source == NULL) || (delimiters == NULL) || (newArg == NULL))
+	{
+		errno = EINVAL;
+		perror("Error!");
+                printf("Errno is %d\n", errno);
+		return -1;
+	}
+
+	snew = source + strspn(source, delimiters);
+
+	if ((buff = malloc(strlen(snew) + 1)) == NULL)
+	{
+		perror("Error!");
+                printf("Errno is %d\n", errno);
+		return -1;
+	}
+
+	strcpy(buff, snew);
+	printf("The buff is %s", buff);
+
+	num_tokens = 0;
+	if(strtok(buff, delimiters) != NULL)
+		for(num_tokens = 1; strok(NULL, delimiters) !+ NULL; num_tokens++);
+
+	if ((*newArg = malloc((num_tokens + 1) * sizeof(char *))) == NULL)
+	{
+		error = errno;
+		free(buff);
+		errno = error;
+		perror("Error!");
+		printf("Errno is %d\n", errno);
+		return -1;
+	}
+
+	else
+	{
+		strcpy(buff, snew);
+
+		**newArg = strtok(buff, delimiters);
+		for (i = 1; i < num_tokens; i++)
+			*((newArg) + i) = strtok(NULL, delimiters);
+	}
+
+	*((*newArg) + num_tokens) = NULL;
+	printf("Token number thingy: %d" num_tokens);
+
+	return num_tokens;
+}
+	
